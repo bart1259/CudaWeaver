337eeb43f869d60851c9ab56408ad16ee0e0eaa9
simongerickson <76057551+simongerickson@users.noreply.github.com>
2022-10-23 19:46:55 -0500

    added define statement to GPUUtils

--- source/GPUUtils.h
+++ source/GPUUtils.h
@@ -1,9 +1,14 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
+#ifndef GPUUtils_H
+#define GPUUtils_H
+
 static void HandleError(cudaError_t err, const char *file,  int line ) { 
 	if (err != cudaSuccess) { 
 		printf("%s in %s at line %d\n", cudaGetErrorString(err),  file, line ); 
 	} 
 } 
 #define HANDLE_ERROR(err) (HandleError( err, __FILE__, __LINE__ )) 
+
+#endif
